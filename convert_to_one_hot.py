3b285d8e8cbde454ca88e3e34440c4567b8ff827
YETI-WU <34374815+YETI-WU@users.noreply.github.com>
2018-05-09 10:10:17 -0500

    Create convert_to_one_hot.py

---
+++ convert_to_one_hot.py
@@ -0,0 +1,6 @@
+# convert_to_one_hot.py  
+  
+def convert_to_one_hot(Y, C):
+    Y = np.eye(C)[Y.reshape(-1)] # reshape(-1) to make an array. np will look at the length of the array and remaining dimensions
+    return Y
+    

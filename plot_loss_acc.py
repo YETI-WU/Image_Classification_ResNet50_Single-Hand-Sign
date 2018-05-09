c87ebd4c0168cb7b7d95671804a9d7ff6c8bcafc
YETI-WU <34374815+YETI-WU@users.noreply.github.com>
2018-05-09 10:12:35 -0500

    Create plot_loss_acc.py

---
+++ plot_loss_acc.py
@@ -0,0 +1,21 @@
+# plot_loss_acc.py  
+  
+import matplotlib.pyplot as plt
+
+# Plot history of loss
+plt.plot(train_history.history['loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train_history_loss'], loc='upper right')
+plt.show()
+
+# plot history of acc
+plt.plot(train_history.history['acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train_history_acc'], loc='upper left')
+plt.show()  
+
+
